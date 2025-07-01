09555734c70b5a0d867ed69ed7c1ae77709ad02f
Milos Milic <mmilic@gmail.com>
2025-07-01 13:13:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "terraform-state-bucket-1cf48a7e-0cce-4afc-b29e-8584937eb9e8"
+    bucket         = "terraform-state-bucket-1cf48a7e-0cce-4afc-b29e-8584937eb9e7"
     key            = "fon-automatizacija/terraform.tfstate"
     region         = "eu-central-1"
     use_lockfile   = true
